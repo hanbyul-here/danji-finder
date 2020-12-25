f63deb54763bc68c7e63a1a5cf475a335cdb0170
Hanbyul Jo <hanbyul@nyu.edu>
2020-12-25 16:21:04 -0500

    global style

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,13 +14,8 @@ export default {
 
 <style>
 html {
-  font-family: 'gtamstandard', Arial, Helvetica, 'Nimbus Sans L', sans-serif;
+  font-family: sans-serif;
   font-size: 18px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
 
